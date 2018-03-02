996523c8d2a910eed0730e4d96fbfbc08e86ad26
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-02 11:29:43 -0500

    added a few things

--- KevinGame/src/character/Player.java
+++ KevinGame/src/character/Player.java
@@ -1,5 +1,13 @@
 package character;
 
-public class Player {
+import java.awt.Image;
+
+import gui.components.*;
+public class Player extends ImageTextButton {
+
+	public Player(String text, Image icon, int x, int y, int width, int height, Action action) {
+		super(text, icon, x, y, width, height, action);
+		// TODO Auto-generated constructor stub
+	}
 
 }
