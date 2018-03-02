223cdaf22eef606f3b10ee8316b956dfb0e655e8
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-02 11:26:32 -0500

    added character and npc

--- KevinGame/src/mainGame/GameScreen.java
+++ KevinGame/src/mainGame/GameScreen.java
@@ -6,12 +6,10 @@ import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
 
 public class GameScreen extends FullFunctionScreen {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8743834112365285160L;
-
 	public GameScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
