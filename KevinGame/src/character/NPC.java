63529c02a5479bfbc731aa747251b333372d3749
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-06 11:36:51 -0500

    things

--- KevinGame/src/character/NPC.java
+++ KevinGame/src/character/NPC.java
@@ -3,12 +3,14 @@ package character;
 import java.awt.Image;
 
 import gui.components.Action;
+import gui.components.ImageTextButton;
 
-public class NPC extends Character{
+public class NPC extends ImageTextButton{
 
 	public NPC(String text, Image icon, int x, int y, int width, int height, Action action) {
 		super(text, icon, x, y, width, height, action);
 		// TODO Auto-generated constructor stub
 	}
+
 
 }
--- KevinGame/src/character/Player.java
+++ KevinGame/src/character/Player.java
@@ -3,12 +3,14 @@ package character;
 import java.awt.Image;
 
 import gui.components.Action;
+import gui.components.ImageTextButton;
 
-public class Player extends Character{
+public class Player extends ImageTextButton{
 
 	public Player(String text, Image icon, int x, int y, int width, int height, Action action) {
 		super(text, icon, x, y, width, height, action);
 		// TODO Auto-generated constructor stub
 	}
+	
 
 }
