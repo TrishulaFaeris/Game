97635f0973965053773017514f89bd73437a58cd
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-03-08 10:46:44 -0500

    things

--- KevinGame/src/character/Player.java
+++ KevinGame/src/character/Player.java
@@ -2,34 +2,55 @@ package character;
 
 import java.awt.Image;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import gui.components.Action;
+import gui.components.AnimatedComponent;
 import gui.components.ImageTextButton;
 import gui.interfaces.KeyedComponent;
 
-public class Player extends ImageTextButton implements KeyedComponent{
+public class Player extends AnimatedComponent implements KeyedComponent{
 
-	public Player(String text, Image icon, int x, int y, int width, int height, Action action) {
-		super(text, icon, x, y, width, height, action);
-		// TODO Auto-generated constructor stub
+	public Player(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			setVx(-3);
+//			setX(getX() - 10);
+//			System.out.println("asdfasd");
+		}
+	    else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+	    	setVx(3);
+	    }
+	    else if (e.getKeyCode() == KeyEvent.VK_UP) {
+	    	setVy(-3);
+		}
+	    else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+	    	setVy(3);
+	    }
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public boolean isHovered(int x, int y) {
 		// TODO Auto-generated method stub
-		
+		return false;
 	}
 
 	@Override
@@ -37,6 +58,6 @@ public class Player extends ImageTextButton implements KeyedComponent{
 		// TODO Auto-generated method stub
 		
 	}
-	
+
 
 }
--- KevinGame/src/mainGame/GameScreen.java
+++ KevinGame/src/mainGame/GameScreen.java
@@ -2,6 +2,7 @@ package mainGame;
 
 import java.util.List;
 
+import character.Player;
 import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
 
@@ -9,6 +10,7 @@ public class GameScreen extends FullFunctionScreen {
 	/**
 	 * 
 	 */
+	public Player p;
 	private static final long serialVersionUID = -8743834112365285160L;
 	public GameScreen(int width, int height) {
 		super(width, height);
@@ -17,7 +19,8 @@ public class GameScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		p = new Player(300, 300, 100, 100);
+		p.addSequence("resources/", time, x, y, w, h, n);
 		
 	}
 
