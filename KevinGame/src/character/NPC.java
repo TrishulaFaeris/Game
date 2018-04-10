b034a007bd89cb59d039427e11b6c48cc5e8d34e
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-04-10 11:34:18 -0400

    npc changes

--- KevinGame/src/character/NPC.java
+++ KevinGame/src/character/NPC.java
@@ -1,15 +1,24 @@
 package character;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import gui.components.Action;
+import gui.components.Component;
 import gui.components.ImageTextButton;
 
-public class NPC extends ImageTextButton{
+public class NPC extends Component{
 
-	public NPC(String text, Image icon, int x, int y, int width, int height, Action action) {
-		super(text, icon, x, y, width, height, action);
+	public NPC(int x, int y, int w, int h) {
+		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
