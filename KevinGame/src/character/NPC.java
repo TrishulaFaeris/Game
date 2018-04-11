3de657243172d280ba8ca26cfdca3d2e2b4e0fbe
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-04-11 11:28:44 -0400

    added images to NPC

--- KevinGame/src/character/NPC.java
+++ KevinGame/src/character/NPC.java
@@ -1,24 +1,31 @@
 package character;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 import gui.components.Action;
+import gui.components.Button;
 import gui.components.Component;
+import gui.components.Graphic;
 import gui.components.ImageTextButton;
 
 public class NPC extends Component{
-
+	
+	private int movement;
 	public NPC(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		// TODO Auto-generated constructor stub
-	}
+		new Graphic(0, 0, "resources/ZFront.png").getImage();
 
+		movement = 10;
+	}
 	@Override
 	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
 		
 	}
-
-
 }
+
+
--- KevinGame/src/mainGame/GameScreen.java
+++ KevinGame/src/mainGame/GameScreen.java
@@ -5,19 +5,18 @@ import java.awt.Rectangle;
 import java.util.List;
 
 import character.Bullet;
+import character.NPC;
 import character.Player;
 import gui.components.Button;
 import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
 
 public class GameScreen extends FullFunctionScreen  {
-	
-
-
 	/**
 	 * 
 	 */
 	private Player p;
+	private NPC c;
 	private static final long serialVersionUID = -8743834112365285160L;
 	public GameScreen(int width, int height) {
 		super(width, height);
@@ -27,10 +26,12 @@ public class GameScreen extends FullFunctionScreen  {
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		p = new Player(300,300,25,25);
+		c = new NPC(100, 100, 25, 25);
 		viewObjects.add(p);
 		for(Bullet b : p.getBullet()) {
 			viewObjects.add(b);
 		}
+		viewObjects.add(c);
 	}
 
 
