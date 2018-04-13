9f3c3fee7dafadf972bdd28172f77247d2df885b
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-04-13 10:31:15 -0400

    remade the images

--- KevinGame/src/character/NPC.java
+++ KevinGame/src/character/NPC.java
@@ -15,9 +15,12 @@ import gui.components.ImageTextButton;
 public class NPC extends Component{
 	
 	private int movement;
+	private BufferedImage[] orientations;
+	
 	public NPC(int x, int y, int w, int h) {
 		super(x, y, w, h);
-		new Graphic(0, 0, "resources/ZFront.png").getImage();
+		orientations = new BufferedImage[4];
+		orientations[0] = new Graphic(0, 0, "resources/ZFront.png").getImage();
 
 		movement = 10;
 	}
@@ -25,6 +28,9 @@ public class NPC extends Component{
 	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
 		
+	}
+	public BufferedImage getImage(){
+		return orientations[0];
 	}
 }
 
